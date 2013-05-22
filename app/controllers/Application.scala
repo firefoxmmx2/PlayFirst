1d5a190f446a22556419576b8fa9ea9d390bce0b
firefoxmmx2 <firefoxmmx@gmail.com>
2013-05-22 16:13:22 +0800

    Action, Controller 和 Result HTTP 路由 操控 Result 会话和 Flash 域

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,16 +6,20 @@ import models.Task
 import play.api.data.Form
 import play.api.data.Forms.nonEmptyText
 import play.api.data.Forms.single
+import play.api.libs.Comet
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.iteratee.Enumeratee
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
 import play.api.mvc.Action
-import play.api.mvc.Controller
 import play.api.mvc.ChunkedResult
+import play.api.mvc.Controller
+import play.api.mvc.Cookie
 import play.api.mvc.ResponseHeader
-import play.api.libs.iteratee.Enumeratee
+import play.api.mvc.ResponseHeader
+import play.api.mvc.SimpleResult
 import play.api.templates.Html
-import play.api.libs.Comet
+import play.api.mvc.Codec
 
 object Application extends Controller {
 
@@ -92,8 +96,7 @@ object Application extends Controller {
     val events = Enumerator(
       """<script>console.log('kiki')</script>""",
       """<script>console.log('foo')</script>""",
-      """<script>console.log('bar')</script>"""
-    )
+      """<script>console.log('bar')</script>""")
     Ok.stream(events >>> Enumerator.eof).as("html")
   }
 
@@ -109,5 +112,108 @@ object Application extends Controller {
   def cometMessage2 = Action {
     val events = Enumerator("kiki", "foo", "bar")
     Ok.stream(events &> Comet(callback = "console.log"))
+  }
+
+  def simpleResult = Action {
+    SimpleResult(
+      header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+      body = Enumerator("Hello World!"))
+  }
+
+  def easyResult = Action {
+    Ok("Hello World1")
+  }
+  val ok = Ok("Hello World")
+  val notFound = NotFound
+  val pageNotFound = NotFound(<h1>Page not found</h1>)
+  //	val badRequest=BadRequest(views.html.form(formWithErrors))	
+  val oops = InternalServerError("Oops")
+  val anyStatus = Status(488)("Strange response type")
+
+  def redirectResult = Action {
+    Redirect("/")
+  }
+
+  //	def redirectResult2 = Action {
+  //		Redirect("/", status = MOVED_PERMANENTLY)
+  //
+  //	}
+
+  val textResult = Ok("Hello World")
+  val xmlResult = Ok(<message>Hello World</message>)
+
+  val htmlResult = Ok(<h1>Hello World1</h1>)
+  val htmlResult2 = Ok(<h1>Hello World</h1>).as(HTML)
+  val htmlResult3 = Ok(<h1>Hello World!</h1>).as("html")
+
+  def httpHeader = Action {
+    Ok("Hello world!").withHeaders(
+      CACHE_CONTROL -> "max-age=3600",
+      ETAG -> "xx")
+  }
+
+  def httpHeader2 = Action {
+    Ok("Hello World!").withHeaders(
+      "author" -> "Unmi",
+      "siteFlag" -> "Hongkong")
+      .withHeaders("siteFlag" -> "USA")
+      .withHeaders("siteFlag" -> "CN")
+
+  }
+
+  //cookie
+  //设置cookie
+  def cookie = Action {
+    Ok("Hello World!").withCookies(
+      Cookie("theme", "blue"))
+  }
+  //废弃cookie
+  def discardCookie = Action {
+    Ok("Hello World!").discardingCookies("theme")
+  }
+
+  implicit val myCustomCharset = Codec.javaSupported("iso-8859-1")
+
+  def changeCharset = Action {
+    Ok(<h1>Hello World!</h1>).as(HTML)
+  }
+
+  //会话
+  def session_ = Action {
+    request =>
+      request.session.get("connected").map {
+        user => Ok("Hello " + user)
+      }.getOrElse(Unauthorized("Oops, you are not connected"))
+  }
+
+  def session_2 = Action {
+    implicit request =>
+      request.session.get("connected") map {
+        user => Ok("Hello " + user)
+      } getOrElse {
+        Unauthorized("Oops, you are not connected")
+      }
+  }
+  //写入session,会替换
+  def saveSession = Action {
+    Ok("Hello World").withSession(
+      "connected" -> "firefoxmmx@gmail.com")
+  }
+  //写入session,添加
+  def appendSession = Action {
+    request =>
+      val session = request.session
+      Ok("Hello World").withSession(session + ("saidHello" -> "yes"))
+  }
+  //写入session,删除
+  def removeSession = Action {
+    request =>
+      val session = request.session
+      Ok("Hello World").withSession(session - "connected")
+  }
+
+  //创造一个全新session,可以废弃以前session
+  def newSession = Action {
+    Ok("Bye").withNewSession
   }
 }
